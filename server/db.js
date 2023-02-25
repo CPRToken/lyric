eb3ecfc401dba3cfecf643c5bccbca4aa0373455
CPRToken <99814986+CPRToken@users.noreply.github.com>
2023-02-25 20:28:36 +0700

    Add files via upload

---
+++ server/db.js
@@ -0,0 +1,21 @@
+import mysql from 'mysql2/promise';
+import fs from 'fs';
+
+
+const pool = mysql.createPool({
+  host: '192.67.255.218',
+  user: 'onlines2_jay',
+  password: 'Ll@ves196214',
+  database: 'onlines2_content',
+});
+
+export async function insertText(filePath) {
+    const connection = await pool.getConnection();
+  
+    try {
+      const text = await fs.promises.readFile(filePath, 'utf-8');
+      await connection.query('INSERT INTO tupac (songs) VALUES (?)', [text]);
+    } finally {
+      connection.release();
+    }
+  }
